cc9855e6b81d6e50139c6c57dd9ea2ca9f10331d
shahadatrucse <shahadat.rucse@gmail.com>
2024-10-26 23:27:57 +0600

    update import data

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,16 +2,15 @@ const express = require("express");
 const tourController = require("../controllers/tourController");
 const router = express.Router();
 
-router.param("id", tourController.checkID);
-
 router
   .route("/")
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 router
   .route("/:id")
   .get(tourController.getTour)
   .patch(tourController.updateTour)
+  .put(tourController.updateTour)
   .delete(tourController.deleteTour);
 
 module.exports = router;
